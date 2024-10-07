421535d415c0cf2e101cf31935a38e8a35cf7ad0
Burak Özgen <132925731+i-burakozgen@users.noreply.github.com>
2024-10-07 21:40:41 +0300

    Initial commit for laptop-2 branch

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import {Flex, Box} from "@chakra-ui/react"
+
+const HomePage = () => {
+  return (
+    <Flex>
+        <Box>
+            {/* sidebar */}
+        </Box>
+        <Box>
+            {/*mainContent */}
+        </Box>
+    </Flex>
+  )
+}
+
+export default HomePage
